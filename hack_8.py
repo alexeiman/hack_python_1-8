aa8e7c6585c356984c47cc6612422711dcca0510
Alexei Pino <alexeipinogruber@gmail.com>
2024-10-28 16:31:16 -0400

    [Feat] Hack 8

---
+++ hack_8.py
@@ -0,0 +1,7 @@
+"""
+list: [1,3,5,7,9] output => [3,5,7]
+"""
+
+def fn_hack_8():
+    result = [1,3,5,7,9]
+    return result[1:4] # pq ponen "#..." arriba si con cambiar esta de aca funciona? de por si no veo otra solucion
